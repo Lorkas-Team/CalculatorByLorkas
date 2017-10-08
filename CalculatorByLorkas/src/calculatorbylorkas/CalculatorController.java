822bef67d0e376ab8b123772d354b181a2d13639
g-Lorkas <giorgoslordopoulos@gmail.com>
2017-10-08 22:13:41 +0300

    Start of master, contains JFrame where u call it from main, and a class named CalculatorController for make methods to controll the jFrame.

---
+++ CalculatorByLorkas/src/calculatorbylorkas/CalculatorController.java
@@ -0,0 +1,16 @@
+package calculatorbylorkas;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lorkas
+ * 
+ */
+public class CalculatorController {
+    
+}
