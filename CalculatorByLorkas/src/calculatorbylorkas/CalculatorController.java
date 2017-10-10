263e90b9e84afd9960dc6c34a3b53fbfdf9b6e57
zah <zah@zah-PC>
2017-10-10 21:08:31 +0300

    Added operators (+ - * /) and method ( setMathAction(String action) ). By zah

--- CalculatorByLorkas/src/calculatorbylorkas/CalculatorController.java
+++ CalculatorByLorkas/src/calculatorbylorkas/CalculatorController.java
@@ -1,5 +1,7 @@
 package calculatorbylorkas;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JTextField;
 
 /*
@@ -7,36 +9,75 @@ import javax.swing.JTextField;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author lorkas
- * 
+ *
  */
 public class CalculatorController {
     //lordopoulos
-    
+
     private JCalculator view;
     private JTextField textIO;
-            
-    public CalculatorController(JCalculator view){
+
+    public CalculatorController(JCalculator view) {
         this.view = view;
         textIO = (this.view).getIO();
-        
+
         //actionsSetUp();
         //keysActionsSetUp();
-        
         (this.view).setVisible(true);
     }
-    
+
     private void keysActionsSetUp() {
-    
+
     }
-    
-    
-     private void actionsSetUp() {
-     
-     
-     }
-    
+
+    private void actionsSetUp() {
+
+        view.addActionPlus(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setMathAction("+");
+            }
+        });
+        view.addActionMinus(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setMathAction("-");
+            }
+        });
+        view.addActionMult(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setMathAction("*");
+            }
+        });
+        view.addActionDiv(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setMathAction("/");
+            }
+        });
+    }
+
+    private void setMathAction(String action) {
+
+        if (textIO.getText().isEmpty() && !action.equals("-")) {
+            return;
+        }
+        if (textIO.getText().length() == 1 && textIO.getText().equals("-")) {
+            return;
+        }
+
+        String text = textIO.getText();
+        if (text.endsWith("/") || text.endsWith("*") || text.endsWith("+") || text.endsWith("-")) {
+            text = text.substring(0, text.length() - 1);
+        }
+        if (text.endsWith(".")) {
+            text = text.substring(0, text.length() - 1);
+        }
+
+        textIO.setText(text + action);
+    }
 }
